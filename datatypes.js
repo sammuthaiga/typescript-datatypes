c85cb94b357f95e91b818739c5449b434c8dae1a
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-04 01:16:41 +0300

    feat: learn typescript datatypes

--- datatypes.js
+++ datatypes.js
@@ -3,3 +3,14 @@ let lname;
 lname = "Sam";
 let newname = lname.toUpperCase();
 console.log(newname);
+let age;
+age = 50;
+console.log(age);
+let dob = "21";
+let result = parseInt(dob);
+let hobby;
+hobby = {
+    hob1: "running",
+    hob2: "coding"
+};
+console.log(hobby);
--- datatypes.ts
+++ datatypes.ts
@@ -1,6 +1,20 @@
 let lname : string;
-lname = "Sam"
+lname = "Sam";
 
-let newname = lname.toUpperCase()
+let newname = lname.toUpperCase();
 
-console.log(newname)
+console.log(newname);
+
+let age : number;
+age = 50;
+console.log(age);
+
+let dob = "21"
+let result = parseInt(dob)
+
+let hobby : object
+hobby = {
+     hob1 : "running",
+     hob2 : "coding"
+}
+console.log(hobby)
