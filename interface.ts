29f4cc8069efc6f38956b410b02f83b4ec065c11
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 23:39:02 +0300

    fix: make some properties optional

---
+++ interface.ts
@@ -0,0 +1,13 @@
+interface user {
+    name: string;
+    age?: number;
+    id:number;
+    email: string;
+}
+
+let user: user = {
+    name: 'John',
+    id: 234567,
+    email: 'john@example.com'
+
+}
