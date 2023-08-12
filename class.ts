c6b3e2b182e47699b921fecc9a248ee71b4a3139
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 22:39:48 +0300

    feat: practice using constructors

--- class.ts
+++ class.ts
@@ -10,3 +10,19 @@ class employee {
  John.address = "23, Nairobi";
 
 console.log(John);
+
+class Customer {
+    id: number;
+    name: string;
+    address: string;
+
+    constructor(id: number, name: string, address: string) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+}
+
+let Sam = new Customer(1, "Sam", "333 Nairobi");
+
+console.log(Sam);
