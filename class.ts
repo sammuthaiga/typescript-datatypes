2eb3c536bf5b262d9db084e7edd2e326de3a1454
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 23:25:53 +0300

    feat:implement private, protected and public classes

--- class.ts
+++ class.ts
@@ -34,3 +34,34 @@ let address = Sam.getNameWithAddress();
 
 console.log(Sam);
 console.log(address);
+
+
+
+class manager {
+    #id: number;
+    protected name: string;
+    address: string;
+
+    constructor(id: number, name: string, address: string) {
+        this.#id = id;
+        this.name = name;
+        this.address = address;
+        }
+        getNameWithAddress() : string {
+            return `${this.name} stays at ${this.address}`;
+    }
+}
+
+class director extends manager {
+    constructor(id: number, name: string, address: string){
+        super(id, name, address);
+    }
+
+    getNameWithAddress(): string {
+        return `${this.name} our director stays at ${this.address}`;
+    }
+}
+
+let Mike = new director(2, "Mike", "Karen, Nairobi");
+
+console.log(Mike.getNameWithAddress())
