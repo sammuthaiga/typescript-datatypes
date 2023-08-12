cd9516409b9efd20de675a3f68c78422d9a31224
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 23:50:32 +0300

    feature: extend interface property and add extra ones

--- interface.ts
+++ interface.ts
@@ -1,3 +1,4 @@
+// constructing a user interface with some requried and optional properties
 interface user {
     name: string;
     // make age an optional property by adding a ? at the end.
@@ -12,3 +13,22 @@ let user: user = {
     email: 'john@example.com'
 
 }
+
+console.log(user);
+
+// Extending an interface and adding some extra properties
+
+interface Employee extends user {
+    // Add the salary property to the inherited properties from user interface
+    salary: number
+}
+
+let emp1: Employee = {
+    name: 'Sam',
+    age: 36,
+    id: 215432674,
+    email: 'sam@example.com',
+    salary: 100000
+}
+
+console.log(emp1)
