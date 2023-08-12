cbc4b616601227ab5def71991726d31fddf03ef1
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 22:32:00 +0300

    feat: practice writting classes

---
+++ class.ts
@@ -0,0 +1,12 @@
+class employee {
+    id!: number;
+    name!: string;
+    address!: string;
+}
+ let John = new employee();
+
+ John.id = 1;
+ John.name = "John";
+ John.address = "23, Nairobi";
+
+console.log(John);
