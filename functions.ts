8d53ea4b8319ad9d08ba99019aab952f5335d8a6
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-12 22:06:17 +0300

    feat: including an rest parameters in your functions logic

--- functions.ts
+++ functions.ts
@@ -22,3 +22,12 @@ const mult = function(num1: number, num2: number): number {
 }
 
 console.log(mult(33,10));
+
+// rest parameters
+
+function add2(num1: number, num2: number, ...num3: number[]): number {
+    return num1 + num2 + num3.reduce((a,b) => a + b,0)
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(add2(2,3, ...numbers))
